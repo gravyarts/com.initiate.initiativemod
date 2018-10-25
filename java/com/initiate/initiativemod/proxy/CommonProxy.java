c8a7f5c433e28fd2f4be48a147cd52c5a9d9a693
Marcogames1973 <44388527+Marcogames1973@users.noreply.github.com>
2018-10-24 22:53:43 -0400

    Delete CommonProxy.java

--- java/com/initiate/initiativemod/proxy/CommonProxy.java
+++
@@ -1,8 +0,0 @@
-package com.initiate.initiativemod.proxy;
-
-import net.minecraft.item.Item;
-
-public class CommonProxy {
-	
-	public void registerItemRenderer(Item item, int meta, String id) {}
-}
